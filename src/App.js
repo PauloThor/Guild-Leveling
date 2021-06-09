4eae0681c4cd4d0d337d0b6430c4bce264fb541d
moresche <renato-moresche@hotmail.com>
2021-06-09 10:15:17 -0300

    bugfix: fixed a minor bug on home route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Routes from "./routes";
+import GlobalStyle from "./styles/global";
 
 const App = () => {
   return (
     <div className="App">
+      <GlobalStyle />
       <Routes />
     </div>
   );
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,12 @@
 import { Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import GlobalStyle from "./styles/global";
 
 const Routes = () => {
   return (
     <Switch>
-      <GlobalStyle />
-      <Navbar />
-      <Route exact path="/"></Route>
+      <Route exact path="/">
+        <Navbar />
+      </Route>
     </Switch>
   );
 };
