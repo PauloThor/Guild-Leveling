aa860d0d10aa45e80c5ad6a3ee17be9ae44771c9
moresche <renato-moresche@hotmail.com>
2021-06-11 10:20:01 -0300

    feature: made few changes on style

--- src/components/FormSign/styles.js
+++ src/components/FormSign/styles.js
@@ -53,7 +53,7 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin-top: 170px;
+    margin-top: 150px;
 
     div.header {
       div.signup-header {
