bb9e9fe4cb8f1317435cb05bf01e3759f8d13e7d
moresche <renato-moresche@hotmail.com>
2021-06-11 12:53:03 -0300

    fix: global colors

--- src/styles/global.js
+++ src/styles/global.js
@@ -15,14 +15,6 @@ export default createGlobalStyle`
         --link: #385898;
         --blue: #007AFF;
 	    --green: #2ade2a;
-        --darkblue: rgba(9, 73, 121, 1);
-        --brown: rgba(90, 64, 84, 1);
-        --gradient-blue-dark: radial-gradient(circle,rgba(9, 73, 121, 1) 0%,rgba(5, 3, 3, 1) 74%);
-        --gradient-brown-dark: radial-gradient(
-            circle,
-            rgba(90, 64, 84, 1) 10%,
-            rgba(5, 3, 3, 1) 93%
-        );
     }
 
     body, div, h1, h2, h3, h4, h5, h6 {
