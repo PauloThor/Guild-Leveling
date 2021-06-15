56f0c1423a1e69a37eab72105fb17925a19abc48
Wander Moreira <wandersantosmoreira@gmail.com>
2021-06-15 08:44:59 -0300

    fix: content update

--- src/provider/index.js
+++ src/provider/index.js
@@ -1,12 +1,15 @@
 import { UserProvider } from "./user";
 import { GuildProvider } from "./guild";
 import { QuestsProvider } from "./quests";
+import { TasksGuildContext } from "./tasksguilds";
 
 const Provider = ({ children }) => {
   return (
     <UserProvider>
       <QuestsProvider>
-        <GuildProvider>{children}</GuildProvider>
+        <GuildProvider>
+          <TasksGuildContext>{children}</TasksGuildContext>
+        </GuildProvider>
       </QuestsProvider>
     </UserProvider>
   );
---
+++ src/provider/tasksguilds/index.js
@@ -0,0 +1,62 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import api from "../../services";
+import { useInfoUser } from "../../provider/user";
+
+const TasksGuildContext = createContext([]);
+
+export const TasksGuildProvider = ({ children }) => {
+  const [tasksFromGuild, setTasksFromGuild] = useState([]);
+  const [task, setTask] = useState({});
+
+  const {
+    infoUser: { access },
+  } = useInfoUser();
+
+  const getQuest = (idQuest) => {
+    api.get(`/goals/${idQuest}/`).then((response) => setTask(response));
+  };
+
+  const getQuestsFromGuild = (idGroup) => {
+    console.log(idGroup);
+    api
+      .get(`/goals/?group=${idGroup}`)
+      .then((response) => setTasksFromGuild(response.data.results));
+  };
+
+  const createQuest = (data) => {
+    api.post("/goals/", data, {
+      headers: `Bearer ${access}`,
+    });
+  };
+
+  const updateQuest = (idQuest) => {
+    api.patch(
+      `goals/${idQuest}/`,
+      {
+        // so mandar a(s) key a ser atualizada
+        title: "Dado a ser alterado",
+        difficulty: "Dado a ser alterado",
+        how_much_achieved: "Dado a ser alterado",
+      },
+      {
+        headers: `Bearer ${access}`,
+      }
+    );
+  };
+
+  const deleteQuest = (idQuest) => {
+    api.delete(`/goals/${idQuest}/`, {
+      headers: `Bearer ${access}`,
+    });
+  };
+
+  return (
+    <TasksGuildContext.Provider
+      value={{ task, tasksFromGuild, getQuestsFromGuild }}
+    >
+      {children}
+    </TasksGuildContext.Provider>
+  );
+};
+
+export const useTasksGuild = () => useContext(TasksGuildContext);
